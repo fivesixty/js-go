745bf1063d50d169cac94715bc3825d534e1c10f
c-spencer <cs560@cam.ac.uk>
2011-04-01 12:14:04 +0100

    Restructure server

--- server.js
+++ server.js
@@ -22,129 +22,168 @@ function htmlEscape(text) {
      replace(/</g,'&lt;');
 }
 
+var ChatRoom = (function () {
+  
+  var ChatRoom = function (name) {
+    this.clients = {};
+    this.name = name;
+  }
+  ChatRoom.prototype = new Object();
+  
+  ChatRoom.prototype.join = function (client) {
+    if (this.clients[client.id] === undefined) {
+      this.clients[client.id] = client;
+      this.broadcast("userjoin", client);
+      client.socket.send(JSON.stringify({
+        type: "userlist",
+        payload: _(this.clients).map(function (client) { return client.idObj(); })
+      }));
+    }
+  }
+  
+  ChatRoom.prototype.leave = function (client) {
+    if (this.clients[client.id] !== undefined) {
+      delete this.clients[client.id];
+      this.broadcast("userleave", client);
+    }
+  }
+  
+  ChatRoom.prototype.broadcast = function (message, from, payload) {
+    var data = {type: message, from: from.idObj()};
+    if (payload !== undefined) {
+      data.payload = payload;
+    }
+    data = JSON.stringify(data);
+    
+    _(this.clients).chain()
+      .select(function (client) { return client.id !== from.id })
+      .each(function (client) { client.socket.send(data); });
+  }
+  
+  ChatRoom.prototype.sendMessage = function (message, from) {
+    this.broadcast("usermessage", from, message);
+  }
+  
+  ChatRoom.prototype.hasClient = function (client) {
+    return (this.clients[client.id] !== undefined);
+  }
+  
+  return ChatRoom;
+  
+}());
+
+
+var GameClient = (function () {
+
+  var GameClient = function (socket, lobby, index) {
+    this.socket = socket;
+    this.id = socket.sessionId;
+    this.name = "Anon User";
+    this.opponent = undefined;
+    this.lobby = lobby;
+    this.index = index;
+    
+    this.index[this.id] = this;
+    
+    socket.send(JSON.stringify({
+      type: "myid",
+      payload: this.id
+    }));
+    
+    socket.on('message', _.bind(function (data) {
+      var message = JSON.parse(data);
+      if (message.type && (typeof this[message.type] === "function")) {
+        this[message.type](message, data);
+      }
+    }, this));
+    
+    socket.on('disconnect', _.bind(function () {
+      this.disconnect();
+    }, this));
+  }
+  GameClient.prototype = new Object();
+  
+  GameClient.prototype.socketSend = function (type, payload) {
+    var data = {type: type};
+    if (payload !== undefined) {
+      data.payload = payload;
+    }
+    this.socket.send(JSON.stringify(data));
+  }
+  
+  GameClient.prototype.setName = function (message) {
+    this.name = htmlEscape(message.payload);
+  }
+  
+  GameClient.prototype.idObj = function () {
+    return {id: this.id, name: this.name};
+  }
+  
+  GameClient.prototype.joinChat = function () {
+    this.lobby.join(this);
+  }
+  
+  GameClient.prototype.leaveChat = function () {
+    this.lobby.leave(this);
+  }
+  
+  GameClient.prototype.sendChat = function (message) {
+    this.lobby.sendMessage(htmlEscape(message.payload), this);
+  }
+  
+  GameClient.prototype.disconnect = function () {
+    this.leaveChat();
+    this.gameleave();
+    delete this.index[this.id];
+  }
+  
+  GameClient.prototype.sendChallenge = function (message) {
+    clientIndex[message.payload.id].socketSend("sendChallenge", this.idObj());
+  }
+  
+  GameClient.prototype.cancelChallenge = function (message) {
+    clientIndex[message.payload.id].socketSend("cancelChallenge", this.idObj());
+  }
+  
+  GameClient.prototype.denyChallenge = function (message) {
+    clientIndex[message.payload.id].socketSend("denyChallenge", this.idObj());
+  }
+  
+  GameClient.prototype.acceptChallenge = function (message) {
+    this.opponent = clientIndex[message.payload.id];
+    this.opponent.opponent = this;
+    this.opponent.socketSend("acceptChallenge", this.idObj());
+  }
+  
+  GameClient.prototype.gamemove = function (message, data) {
+    if (this.opponent) {
+      this.opponent.socketSend("gamemove", message.payload);
+    }
+  };
+  
+  GameClient.prototype.gamemessage = function (message) {
+    if (this.opponent) {
+      this.opponent.socketSend("gamemessage", htmlEscape(message.payload));
+    }
+  };
+  
+  GameClient.prototype.gameleave = function () {
+    if (this.opponent) {
+      this.opponent.socketSend("gameleave");
+      this.opponent.game = undefined;
+    }
+    this.game = undefined;
+  };
+  
+  return GameClient;
+
+}());
+
 // socket.io 
 var socket = io.listen(server);
+var clientIndex = {}
+var gameLobby = new ChatRoom("lobby");
 
-function getChatters(callback) {
-  var chatters = _(socket.clients).chain()
-    .select(function (client) { return client.inChat === true; })
-    .map(function (client) { return callback(client.sessionId, client); })
-    .value();
-  return chatters;
-}
-
-function broadcastToChatters(message, from, payload) {
-  var data = JSON.stringify({
-    type:message,
-    from: {id: from.sessionId, name: from.name}
-  });
-  
-  if (payload !== undefined) {
-    data.payload = payload;
-  }
-  
-  _(socket.clients).chain()
-    .select(function (client) { return client.inChat === true && client.sessionId !== from.sessionId })
-    .each(function (client) { client.send(data); });
-}
-
-function challengeMessage(target, sender, type) {
-  target.send(JSON.stringify({
-    type: type,
-    payload: {id:sender.sessionId, name:sender.name}
-  }));
-}
-
-function acceptChallenge(target, sender) {
-  var game = {black: sender, white: target};
-  sender.game = game;
-  target.game = game;
-  
-  challengeMessage(target, sender, "acceptChallenge");
-}
-
-function withOpponent(client, callback) {
-  if (client.game) {
-    if (client.game.black.sessionId === client.sessionId) {
-      callback(client.game.white);
-    } else {
-      callback(client.game.black);
-    }
-  }
-}
-
-socket.on('connection', function(client) { 
-  
-  client.inChat = false;
-  
-  client.send(JSON.stringify({
-    type: "myid",
-    payload: client.sessionId
-  }));
-  
-  client.on('message', function(data) {
-    var message = JSON.parse(data);
-    
-    switch(message.type) {
-      case "setName":
-        client.name = htmlEscape(message.payload);
-        break;
-      case "joinChat":
-        client.inChat = true;
-        broadcastToChatters("userjoin", client);
-        client.send(JSON.stringify({
-          type: "userlist",
-          payload: getChatters(function (id, client) {
-            return {id:id,name:client.name};
-          })
-        }));
-        break;
-      case "leaveChat":
-        client.inChat = false;
-        broadcastToChatters("userleave", client);
-        break;
-      case "sendChat":
-        broadcastToChatters("usermessage", client, htmlEscape(message.payload));
-        break;
-        
-      case "sendChallenge":
-        challengeMessage(socket.clients[message.payload.id], client, "sendChallenge");
-        break;
-      case "cancelChallenge":
-        challengeMessage(socket.clients[message.payload.id], client, "cancelChallenge");
-        break;
-      case "denyChallenge":
-        challengeMessage(socket.clients[message.payload.id], client, "denyChallenge");
-        break;
-      case "acceptChallenge":
-        acceptChallenge(socket.clients[message.payload.id], client);
-        break;
-      case "gamemove":
-        withOpponent(client, function (opponent) {
-          opponent.send(data);
-        });
-        break;
-      case "gamemessage":
-        withOpponent(client, function (opponent) {
-          opponent.send(data);
-        });
-        break;
-      case "gameleave":
-        withOpponent(client, function (opponent) {
-          opponent.send(data);
-          opponent.game = undefined;
-        });
-        client.game = undefined;
-        break;
-    }
-    
-  });
-  client.on('disconnect', function(){
-    client.inChat = false;
-    withOpponent(client, function (opponent) {
-      opponent.send(JSON.stringify({type:"gameleave"}));
-      opponent.game = undefined;
-    });
-    broadcastToChatters("userleave", client);
-  });
+socket.on('connection', function(client) {
+  new GameClient(client, gameLobby, clientIndex);
 });
